22f8d8b3c622254c446d34408d8be9eddce385e1
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-16 16:47:38 -0700

    putc function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- *main
+ * main - Entry point
  *
- *return 0 -> success compilation
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
