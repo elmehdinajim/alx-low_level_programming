9266e94c18ade452c22c867984144f655fd214d6
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-17 16:27:40 -0700

    abcABC ...

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+
+	}
+
+	for (a = 'A' ; a <= 'Z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
