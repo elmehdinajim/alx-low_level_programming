39250c8e2791e3db5a6ac12798804f79b65f48e4
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-17 16:29:46 -0700

    abcABC ...

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		if ((a == 'q' || a == 'e') != 1)
+		{
+			putchar(a);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
