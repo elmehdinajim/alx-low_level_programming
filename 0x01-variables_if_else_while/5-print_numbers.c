d627a3d4822da2805338bcc58fd6a66181afbc3e
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-17 16:31:52 -0700

    0098765432

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		printf("%d", na;
+	}
+	putchar('\n');
+	return (0);
+}
