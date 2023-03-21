0bd0f6f16d60eb51c5ab2b8fcd6606cb25b48dbf
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-20 19:48:23 -0700

    if else

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,28 @@
 int main(void)
 {
 
-	int a,b;
+        int a,b;
 
-	for (a = 0 ; a <=99 ; a++)
-	{
+        for (a = 0 ; a <=99 ; a++)
+        {
 
-		for (b = 0 ; b <= 99 ; b++)
-		{
-			if (a<b && a != b)
-			{
-				putchar((a/10)+'0');
-				putchar((a%10)+'0');
-				putchar(' ');
-				putchar((b/10)+'0');
-				putchar((b%10)+'0');
-				if (a != 98 || b != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+                for (b = 0 ; b <= 99 ; b++)
+                {
+                        if (a<b && a != b)
+                        {
+                                putchar((a/10)+'0');
+                                putchar((a%10)+'0');
+                                putchar(' ');
+                                putchar((b/10)+'0');
+                                putchar((b%10)+'0');
+                                if (a != 98 || b != 99)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
