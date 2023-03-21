33c20c6a0133e136098275ec82b85bed55b9e4bc
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-20 19:54:12 -0700

    if else

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int a, b;
 
-        for (a = 0 ; a <=99 ; a++)
-        {
+	for (a = 0 ; a <=99 ; a++)
+	{
 
-                for (b = 0 ; b <= 99 ; b++)
+		for (b = 0 ; b <= 99 ; b++)
                 {
                         if (a<b && a != b)
                         {
