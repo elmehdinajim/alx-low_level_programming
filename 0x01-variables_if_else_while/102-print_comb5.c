de738b8ef2fe95ebb39e7a7937f090db12b94c75
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-20 19:34:58 -0700

    if else

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,14 +15,14 @@ for ( a = 0 ; a <=99 ; a++)
 
 	for (b = 0 ; b <= 99 ; b++)
 	{
-        if (a<j && i != b)
+        if (a<b && a != b)
         {
             putchar((a/10)+'0');
-            putchar((i%10)+'0');
+            putchar((a%10)+'0');
             putchar(' ');
-            putchar((a/10)+'0');
+            putchar((b/10)+'0');
             putchar((b%10)+'0');
-            if (i != 98 || b != 99)
+            if (a != 98 || b != 99)
             {
                 putchar(',');
                 putchar(' '); 
