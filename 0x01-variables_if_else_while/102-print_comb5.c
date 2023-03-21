545f0879b3771bdc2dc5f6e6fcc2d514275bae54
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-20 19:40:24 -0700

    if else

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,28 +7,29 @@
 
 int main(void)
 {
+
 	int a,b;
 
-for ( a = 0 ; a <=99 ; a++)
-{	
-
-	for (b = 0 ; b <= 99 ; b++)
+	for (a = 0 ; a <=99 ; a++)
 	{
-        if (a<b && a != b)
-        {
-            putchar((a/10)+'0');
-            putchar((a%10)+'0');
-            putchar(' ');
-            putchar((b/10)+'0');
-            putchar((b%10)+'0');
-            if (a != 98 || b != 99)
-            {
-                putchar(',');
-                putchar(' '); 
-            }
-          }
+
+		for (b = 0 ; b <= 99 ; b++)
+		{
+			if (a<b && a != b)
+			{
+				putchar((a/10)+'0');
+				putchar((a%10)+'0');
+				putchar(' ');
+				putchar((b/10)+'0');
+				putchar((b%10)+'0');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
-}
 	putchar('\n');
 	return (0);
 }
