7a2662c1de09146d21ce17b3d8a41183422b5d82
“elmehdinajim” <najimelmehdi42@gmail.com>
2023-03-21 23:06:25 -0400

    last test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,17 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
-		{
-			for (j = 0; j < 10; j++)
-				{
-					k = j * i;
-						if (j == 0)
-							{
-								_putchar(k + '0');
-							}
+for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+			{
+				k = j * i;
+					if (j == 0)
+						{
+							_putchar(k + '0');
+						}
 
-						if (k < 10 && j != 0)
+					if (k < 10 && j != 0)
 						{
 							_putchar(',');
 							_putchar(' ');
@@ -31,7 +31,7 @@ void times_table(void)
 							_putchar((k / 10) + '0');
 							_putchar((k % 10) + '0');
 						}
-					}
-				_putchar('\n');
-		}
+				}
+		_putchar('\n');
+	}
 }
