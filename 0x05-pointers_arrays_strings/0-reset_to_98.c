6a5ef26d259cf1cd20b2b91d884c61c6593c49d4
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-27 13:10:19 -0400

    Pointers and array task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an integer 
+ * reset_to_98 - function that takes a pointer to an integer
  * @n: integer pointer
  * Return: 0 Success
  */
