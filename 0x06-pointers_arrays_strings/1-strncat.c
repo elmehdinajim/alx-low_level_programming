047bb3ac2df8ca38f961159cbf76a54d985c1511
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-31 17:58:43 -0400

    more pointers X task_Pff

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,29 @@
 #include "main.h"
 /**
- *_strncat - concatenates two string
- *@dest: A pointer to a character to be changed
- *@src: A pointer to a character to be changed
- *@n: value
- *Return: dest
+ * _strncat - concatenate two strings
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
+        int i;
         int j;
 
+        i = 0;
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
         j = 0;
         while (j < n && src[j] != '\0')
         {
-                dest[j] = src[j];
-                j++;
+        dest[i] = src[j];
+        i++;
+        j++;
         }
-        while (j < n)
-        {
-                dest[j] = '\0';
-                j++;
-        }
-
+        dest[i] = '\0';
         return (dest);
 }
