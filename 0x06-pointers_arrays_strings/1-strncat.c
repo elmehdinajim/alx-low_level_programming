ac0b8d740cbfab9baa3e352701ab0868705187fa
elmehdinajim <el.mehdi.najim@gmail.com>
2023-03-31 18:00:02 -0400

    more pointers X task_Pff

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncat - concatenate two strings
  * @dest: input value
@@ -7,23 +8,20 @@
  *
  * Return: dest
  */
-char *_strncat(char *dest, char *src, int n)
-{
-        int i;
-        int j;
+char * _strncat(char * dest, char * src, int n) {
+  int i;
+  int j;
 
-        i = 0;
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
-        j = 0;
-        while (j < n && src[j] != '\0')
-        {
-        dest[i] = src[j];
-        i++;
-        j++;
-        }
-        dest[i] = '\0';
-        return (dest);
+  i = 0;
+  while (dest[i] != '\0') {
+    i++;
+  }
+  j = 0;
+  while (j < n && src[j] != '\0') {
+    dest[i] = src[j];
+    i++;
+    j++;
+  }
+  dest[i] = '\0';
+  return (dest);
 }
