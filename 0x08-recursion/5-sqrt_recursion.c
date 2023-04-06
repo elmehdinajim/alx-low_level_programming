3783a8a1423e242c30a922c8fcdab60edad45385
elmehdinajim <el.mehdi.najim@gmail.com>
2023-04-05 20:17:43 -0400

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+
+/**
+ *_sqrt_recursion - nat square root of n
+ *@n: square root of
+ *
+ *Return: results
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ *actual_sqrt_recursion - natural square root of a n
+ *@n: sqaure root of
+ *@i: iterator
+ *
+ *Return: result
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
