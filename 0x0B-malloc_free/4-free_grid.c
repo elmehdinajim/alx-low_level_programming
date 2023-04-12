bd5bde5c974a05732ae8a76c4c5784df79dca9a9
elmehdinajim <el.mehdi.najim@gmail.com>
2023-04-11 20:20:00 -0400

    mem allocation tasks

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *free_grid - 2d array
+ *@grid: 2d grid
+ *@height: height of grid
+ *Description: frees memory of grid
+ *Return: -
+ *
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
