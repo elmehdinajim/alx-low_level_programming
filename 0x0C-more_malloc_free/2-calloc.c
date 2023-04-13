37d09a74e9be81e3d1ebda77b9d30556d0c570e3
elmehdinajim <el.mehdi.najim@gmail.com>
2023-04-13 14:04:07 -0400

    more malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_calloc -allocated memo for nmemb de size bytes
+ *@nmemb: elements in array
+ *@size: bytes of each position in the array
+ *Return: pointer 
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb *size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
+
+	return (p);
+
+}
