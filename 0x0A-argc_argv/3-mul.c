8f94efd89eb28fbbdf29a3c0df2b537383821942
elmehdinajim <el.mehdi.najim@gmail.com>
2023-04-15 15:29:39 -0400

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -27,9 +27,9 @@ int _atoi(char *s)
 			nmbr = s[i] - '0';
 			if (d % 2)
 				nmbr = -nmbr;
-			n = n * 10 + nmbr;
+			n = n *10 + nmbr;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (s[i + 1]<'0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
@@ -40,4 +40,27 @@ int _atoi(char *s)
 	if (f == 0)
 		return (0);
 	return (n);
+}
+
+/**
+ *main - two numbers multiplication
+ *@argc: number of args
+ *@argv: array of args
+ *Return: 0, 1
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
 }
